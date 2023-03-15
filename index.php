937c05f4dff8ff6f8f7463f3949407eddf840cdc
komangraditya11 <127906543+komangraditya11@users.noreply.github.com>
2023-03-15 09:58:52 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>latihan 1</title>
+</head>
+<body>
+    <h3>latihan web progamming </h3>
+    <script>
+        document.write("latihan web progamming - dg javascrip")
+    </script>
+
+  <br>
+
+  
+<?php
+   echo "Latihan web progamming  - dg php";
+?>
+    
+</body>
+</html>
