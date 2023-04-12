64d509fa1d3d19e437d47a3eb929d20b578c7139
komangraditya11 <127906543+komangraditya11@users.noreply.github.com>
2023-04-12 09:56:28 +0800

    Add files via upload

---
+++ sesi5/DATABASE/koneksi.php
@@ -0,0 +1,11 @@
+<?php
+        include("../konfigurasi.php");
+        $cnn = mysqli_connect(DBHOST,DBUSER,DBPASS,DBNAME,DBPORT) or die ("koneksi ke DBMS mysql gagal");
+        
+        // if($cnn){
+        //     echo"koneksi ke DBMS mysql sukses";
+        // }else{
+        //     echo"koneksi ke DBMS mysql gagal";
+        // }
+        
+        // mysqli_close($cnn);
